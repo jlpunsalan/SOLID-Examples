73d38c22456d0ae4360818b1bf1f3a87fc088c47
Jeremy Punsalan <jem.punsalan@yahoo.com>
2018-11-25 14:09:28 +0800

    First Commit

---
+++ src/test/java/com/affinityjava/solidexample/SolidexampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.affinityjava.solidexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SolidexampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
